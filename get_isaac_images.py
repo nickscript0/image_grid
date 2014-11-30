fbec1acfacaace2293c72fbf74ef1a30a6aaa4b0
nickscript0 <nocl123@gmail.com>
2014-11-30 16:42:32 -0700

    switched single JSON file containing descriptions

--- get_isaac_images.py
+++ get_isaac_images.py
@@ -3,6 +3,7 @@ import urllib
 import os
 import os.path
 import re
+import json
 
 import BeautifulSoup
 
@@ -11,34 +12,78 @@ BASE_URL = 'http://bindingofisaacrebirth.gamepedia.com'
 MAIN_DIV_ID = "mw-content-text"
 
 RESOURCE_PATH = 'dev/isaac_grid/res'
+DESCRIPTIONS_FILE = 'descriptions.json'
 
 
-
+        
 def save_tables(bs=None):
     TABLE1_ID = "wikitable"
     
     if bs is None:
         bs = bs_from_url(BASE_URL+'/Collection_Page')
-    
+        
     # Loop through all wikitables on page
     div1 = bs.find('div', attrs={'id': MAIN_DIV_ID})
     current_table = div1.findNext('table', attrs={'class': TABLE1_ID})
     i = 1
-    while(current_table != None):
-        tds = current_table.findAll('td')
-        #save_table(tds)
-        debug('save_table ' + str(i))
-        current_table = current_table.findNextSibling('table', attrs={'class': TABLE1_ID})
-        i += 1
+    try:
+        while(current_table != None):
+            tds = current_table.findAll('td')
+            desc_file = Desc()
+            save_table(tds, desc_file)
+            debug('Processing table ' + str(i))
+            current_table = current_table.findNextSibling('table', attrs={'class': TABLE1_ID})
+            i += 1
+    finally:
+        desc_file.write()
     
-    
-def save_table(table_tds):
+def save_table(table_tds, desc_file):
     already_saved = get_already_saved()
     # Loop through each table <td>
     for td in table_tds:
         image_name = image_name_from_url(td.find('img')['src']).split('.')[0]
         if image_name not in already_saved:
-            save_td(td)
+            save_td(td, desc_file)
+        
+def save_td(td, desc_file):
+    # Get image
+    img = td.find('img')
+    img_url = img['src']
+    save_image(img_url)
+    
+    # Get description
+    image_name = image_name_from_url(img_url).split('.')[0]
+    desc_rel_url = td.find('a')['href'] # Description page url
+    save_description(desc_rel_url, image_name, image_formatted_name, desc_file)
+
+def save_image(img_url):
+    path = os.path.join(RESOURCE_PATH, image_name_from_url(img_url))
+    urllib.urlretrieve(img_url, path)
+    debug('Saved '+path)
+    
+def save_description(desc_relative_url, image_name, desc_file):
+    #path = os.path.join(RESOURCE_PATH, name)
+    #open(path, 'w').write(get_description(desc_relative_url))
+    desc_file.current[image_name] = {'description': get_description(desc_relative_url)}
+
+### Helpers
+class Desc(object):
+    """
+    Loads the pre-existing description JSON from file, allowing app
+    to write to it, then write it back to file.
+    """
+    path = os.path.join(RESOURCE_PATH, DESCRIPTIONS_FILE)
+    
+    def __init__(self):
+        try:
+            self.current = json.loads(open(self.path).read())
+        except IOError:
+            # File doesn't exist yet
+            self.current = {}
+
+    def write(self):
+        """ Writes the current JSON back to file. """
+        open(self.path, 'w').write(json.dumps(self.current))
 
 def get_already_saved():
     """
@@ -46,27 +91,7 @@ def get_already_saved():
     """
     files = os.listdir(RESOURCE_PATH)
     unique_names = set([x.split('.')[0] for x in files if x.find('.') != -1])
-    return unique_names
-        
-def save_td(td):
-    # Get image
-    img_url = td.find('img')['src']
-    save_image(img_url)
-    
-    # Get description
-    desc_filename = image_name_from_url(img_url).split('.')[0] + '.txt'
-    desc_rel_url = td.find('a')['href'] # Description page url
-    save_description(desc_rel_url, desc_filename)
-
-def save_image(img_url):
-    path = os.path.join(RESOURCE_PATH, image_name_from_url(img_url))
-    urllib.urlretrieve(img_url, path)
-    debug('Saved '+path)
-    
-def save_description(desc_relative_url, name):
-    path = os.path.join(RESOURCE_PATH, name)
-    open(path, 'w').write(get_description(desc_relative_url))
-    debug('Saved '+path)
+    return unique_names    
     
 def get_description(desc_relative_url):
     """
