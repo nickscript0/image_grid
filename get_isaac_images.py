15b190851a53c4d6a4b7c8b2d0dcf22754acebd6
nickscript0 <nocl123@gmail.com>
2014-11-30 08:48:07 -0700

    Working get_description function, also update gitignore for pyc

--- get_isaac_images.py
+++ get_isaac_images.py
@@ -1,26 +1,27 @@
 import BeautifulSoup
 import urllib2
 
-BASE_URL = 'http://bindingofisaacrebirth.gamepedia.com/Collection_Page'
+# BOI wiki constants
+BASE_URL = 'http://bindingofisaacrebirth.gamepedia.com'
+MAIN_DIV_ID = "mw-content-text"
+
 RESOURCE_PATH = 'scripts/isaac_grid/res'
 
 def get_table(bs=None):
-    DIV1_ID = "mw-content-text"
     TABLE1_ID = "wikitable"
     
     if bs is None:
-        res = make_request(BASE_URL)
-        bs = BeautifulSoup.BeautifulSoup(res)
+        bs = bs_from_url(BASE_URL+'/Collection_Page')
     
     # Parse one table
     # TODO parse all six tables
-    div1 = bs.find('div', attrs={'id': DIV1_ID})
+    div1 = bs.find('div', attrs={'id': MAIN_DIV_ID})
     t1 = div1.find('table', attrs={'class': TABLE1_ID})
     els = t1.findAll('td')
     
     # Get image
     img_url = els[0].find('img')['src'] # Image path
-    urllib.urlretrieve(els[0].find('img')['src'], RESOURCE_PATH+parse_image_name_from_url(img_url))
+    urllib.urlretrieve(els[0].find('img')['src'], RESOURCE_PATH+image_name_from_url(img_url))
     
     # Get description
     els[0].find('a')['href'] # Description page url
@@ -29,17 +30,28 @@ def get_table(bs=None):
     #debug('div1=%s' %(div1))
     return t1
             
-            
-def make_request(url):
+def get_description(desc_relative_url):
+    bs = bs_from_url(BASE_URL+desc_relative_url)
+    
+    # Location: The first <p> after the first <h2> in MAIN_DIV
+    div1 = bs.find('div', attrs={'id': MAIN_DIV_ID})
+    return div1.find('h2').findNext('p').text
+    
+def bs_from_url(url):
+    """
+    Given a url string requests and returns the corresponding BS object.
+    """
+    # Non-browser user-agent was giving 403 reponse
     req = urllib2.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    return urllib2.urlopen(req).read()
+    res = urllib2.urlopen(req).read()
+    return BeautifulSoup.BeautifulSoup(res)
 
-def parse_image_name_from_url(s):
+def image_name_from_url(s):
     """
     Converts: http://hydra-media.cursecdn.com/bindingofisaacrebirth.gamepedia.com/1/1a/The_Sad_Onion_Icon.png?version=9b2daa8d246033e451ae7527dae14f9d
     To: The_Sad_Onion_Icon.png
     
-    *Not safe, doesn't use urllib encoding just simple string search*
+    *Not robust, doesn't use urllib encoding just simple string search*
     """
     if s.find('?') != -1:
         s = s.split('?')[0]
