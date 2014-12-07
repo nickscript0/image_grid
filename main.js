b3af233041fb09b0d39d2b96c870373f643db7c6
nickscript0 <nocl123@gmail.com>
2014-12-07 13:49:39 -0700

    refactor to better split view and model logic

--- main.js
+++ main.js
@@ -3,57 +3,56 @@ var ig = {};
 
 
 // Models
+ig.items = function (items_json) {
+    // List of {image_url, description, name} objects
+    this.list = m.prop([]);
+    // Map of key=name, value=description
+    this.dict = m.prop({});
+    
+    for (var key in items_json) {
+        if (items_json.hasOwnProperty(key)) {
+            // Escape % by appending '25', due to how python SimpleHTTPServer serves files
+            escaped_key = key.split('%').join('%25');
+            this.list().push( {image_url: '/res/'+escaped_key, 
+                              description: items_json[key].description,
+                              name: items_json[key].name} );
+            this.dict()[items_json[key].name] = items_json[key].description;
+        }
+    }   
+};
+
+// This is not a standalone model as it references ig.vm.items, it is a vm helper
+// TODO: could be refactored using a closure/currying and accept the items.dict as the first arg.
+ig.addTooltip = function (element, isInitialized, context) {
+    if (isInitialized) return;
+    var name = element.alt; // img.alt
+    var desc = ig.vm.items().dict()[element.alt];
+    var text = '<h3>'+name+'</h3>'+desc;
+
+    var myOpentip = new Opentip(element, text, {
+        style: "dark", // Others: alert, dark, glass, drop
+        showOn: 'mouseover',
+        tipJoint: "top"
+    });            
+};    
+
+
 ig.vm = new function () {
     var vm = {};
     
-    // key: name, description
-    vm.items_map = m.prop({});
+    // ig.items
+    vm.items = m.prop({});
     
     // Init called by controller
     vm.init = function () {
         // Get items list
-        vm.items = m.prop([]);
-        m.request({method: "GET", url: "/res/descriptions.json"}).then(vm.itemsToUrls).then(vm.items);
+        vm.items = m.prop({});
+        
+        m.request({method: "GET", url: "/res/descriptions.json"}).then(function (a) {
+            vm.items(new ig.items(a))
+        });
         
     };
-    
-    // Converts the 'items' object to an Array of image urls
-    vm.itemsToUrls = function (items) {
-        var items_list = [];
-        for (var key in items) {
-            if (items.hasOwnProperty(key)) {
-                // Escape % by appending '25', due to how python SimpleHTTPServer serves files
-                escaped_key = key.split('%').join('%25');
-                items_list.push( {image_url: '/res/'+escaped_key, 
-                                  description: items[key].description,
-                                  name: items[key].name} );
-                ig.vm.items_map()[items[key].name] = items[key].description;
-            }
-        } 
-        return items_list;
-    }
-
-    // Builds a hover text block
-    vm.buildHover = function (item) {   
-        return m("div.item_block", [
-            m("img.wikitable", {config: ig.vm.addTooltip, src: item.image_url,
-                               alt: item.name}),
-        ])
-    }
-    
-    vm.addTooltip = function (element, isInitialized, context) {
-        if (isInitialized) return;
-        var name = element.alt; // img.alt
-        var desc = ig.vm.items_map()[element.alt];
-        var text = '<h3>'+name+'</h3>'+desc;
-
-        var myOpentip = new Opentip(element, text, {
-            style: "dark", // Others: alert, dark, glass, drop
-            showOn: 'mouseover',
-            tipJoint: "top"
-        });            
-    };
-    
     return vm
 }
 
@@ -62,21 +61,29 @@ ig.controller = function() {
     ig.vm.init()
 }
 
-// View
+// Views
 ig.view = function() {
     return m("html", [
         m("body", [
             m("div", [
-                ig.vm.items().map(function (item, index){
+                ig.vm.items().list().map(function (item, index){
                     // TODO: if we want a nice uniform grid look with no vertical spacing
                     // we should use a table with overflow set
-                    return ig.vm.buildHover(item)
+                    return ig.hover(item)
                 })
             ]),
-            m("div", "Item Count: " + ig.vm.items().length)
+            m("div", "Item Count: " + ig.vm.items().list().length)
         ])
     ]);
 };
+
+// Builds a hover text block
+ig.hover = function (item) {   
+    return m("div.item_block", [
+        m("img.wikitable", {config: ig.addTooltip, src: item.image_url,
+                           alt: item.name}),
+    ])
+}
 
 //initialize the application
 m.module(document.getElementById("ig_app"), {controller: ig.controller, view: ig.view});
